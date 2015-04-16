cd6abaa1770a8e0dd090932558b5a85251415463
root <root@anzhy.centOS>
2015-04-16 23:52:31 +0800

    add all methods without Main test

--- bit_map/bit_map.cpp
+++ bit_map/bit_map.cpp
@@ -67,3 +67,62 @@ int Bitmap::get_bit_map_value ( int index )
   
    return -1 ; // this branch could be reach 
 }
+
+int Bitmap::set_bit_map_value ( int index, int set_value )
+{
+   // index legal testing , incr by bits 
+
+   if ( index < 0 || index >bit_length  )
+   {
+	LOG(WARNING)<<"[warning] index illegal range";
+	return -1 ;
+   } 
+
+  // then set_value
+   
+   if ( set_value != 0 && set_value != 1 )
+   {
+	LOG(WARNING)<<"[warning] input set_value error ";
+	return -1 ;
+   }
+
+   int pos_byte = index / 8 ;
+   int pos_bit  = index % 8 ;
+   
+   // now divide two branches by set_value = 1 or 0
+   if ( set_value == 1)
+   {
+	// call set_value_one 
+   	set_value_one(bit_field[pos_byte],pos_bit ) ;
+	
+   }
+   
+   if ( set_value == 0 ) 
+   {
+	// call set_value_zero
+  	set_value_zero(bit_field[pos_byte],pos_bit) ;
+   } 
+
+  
+   return 1 ;
+}
+
+
+void Bitmap::print ()
+{
+  // this method is used to output each bit in each unsigned char 
+  // as element stored in each vector<char> bit_field ;
+	
+  for ( int i = 0 ; i < bit_field.size() ;i++  )
+  {
+	for ( int j = 0 ; j < 7 ; j++ ) // each unsigned char has 8 bits [0...7]
+ 	{
+		if ( (bit_field[i] >> j) &1 )
+		   printf ("1") ;
+		else
+		   printf ("0") ;
+	} 
+
+ }
+}
+
